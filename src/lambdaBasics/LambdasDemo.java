c2c295fd7dfd761bf2d5b26920433779bb1f8131
Jessica C <jcake2@gmail.com>
2015-06-29 09:11:31 -0500

    Adding more demo setup so button is displayed

--- src/lambdaBasics/LambdasDemo.java
+++ src/lambdaBasics/LambdasDemo.java
@@ -4,11 +4,18 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class LambdasDemo {
 
 	public static void main(String[] args){
+		JFrame frame = new JFrame();
+		frame.setBounds(100, 100, 80, 80);
+		JPanel panel = new JPanel();
+		frame.add(panel);
 		JButton someButton = new JButton("Click Me");
+		panel.add(someButton);
 		
 		//Pre-Java 8
 		someButton.addActionListener(new ActionListener(){
@@ -19,6 +26,8 @@ public class LambdasDemo {
 		});
 		
 		//Java 8 Lambdas:
-		someButton.addActionListener(event -> System.out.println("You performed event: " + event.getActionCommand()));
+//		someButton.addActionListener(event -> System.out.println("You performed event: " + event.getActionCommand()));
+		
+		frame.setVisible(true);
 	}
 }
